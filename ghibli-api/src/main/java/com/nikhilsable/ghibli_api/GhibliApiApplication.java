73861126863ede9f3e02512c77ecffd4127d905d
nikhisable5050 <nikhilsable5050@gmail.com>
2026-02-02 13:12:50 +0530

    Add Feign configuration for multipart form-data encoder

--- ghibli-api/src/main/java/com/nikhilsable/ghibli_api/GhibliApiApplication.java
+++ ghibli-api/src/main/java/com/nikhilsable/ghibli_api/GhibliApiApplication.java
@@ -2,8 +2,10 @@ package com.nikhilsable.ghibli_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class GhibliApiApplication {
 
 	public static void main(String[] args) {
---
+++ ghibli-api/src/main/java/com/nikhilsable/ghibli_api/config/FeignConfig.java
@@ -0,0 +1,21 @@
+package com.nikhilsable.ghibli_api.config;
+
+
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public Encoder feignFormEncoder() {
+        return new SpringFormEncoder(
+                new SpringEncoder(() -> new HttpMessageConverters(new RestTemplate().getMessageConverters()))
+        );
+    }
+}
