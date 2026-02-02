8bcc26f83f405ebea95fbd34384058aaa87e46f8
nikhisable5050 <nikhilsable5050@gmail.com>
2026-02-02 12:15:46 +0530

    Initial backend commit

---
+++ ghibli-api/src/main/java/com/nikhilsable/ghibli_api/GhibliApiApplication.java
@@ -0,0 +1,13 @@
+package com.nikhilsable.ghibli_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhibliApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhibliApiApplication.class, args);
+	}
+
+}
---
+++ ghibli-api/src/test/java/com/nikhilsable/ghibli_api/GhibliApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhilsable.ghibli_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhibliApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
